49f0daeb311eb4406a009412cdcf4dd8fe8596d0
nnikhil <nikhil_narendran@hotmail.com>
2022-02-06 22:07:49 -0500

    added connections in server.js

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-//this is server.js
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+const mysqlpromise = require("mysql2/promise");
+const { dbConnect, db } = require("./config/connection");
+
+// To establish connection with database
+db.connect((err) => {
+  if (err) throw err;
+  console.log("Connection Estabilished");
+  listMenu();
+});
